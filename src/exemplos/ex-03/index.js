e85803f8eb53ef4948cbe3a029495631f1c7a6b5
palomo2005 <gabriel.palomo@etec.sp.gov.br>
2025-04-02 20:39:41 -0300

    Aula 02/04 2

--- src/exemplos/ex-03/index.js
+++ src/exemplos/ex-03/index.js
@@ -1,20 +1,42 @@
-import { View, Text, Button } from 'react-native'; 
+import { View, Text, Button, Alert, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
-function Exemplo3 () {
-    return(
+function Exemplo3() {  
+
+    function handleExibeMensagem () {
+        Alert.alert('Teste')
+    }
+
+    const handleOutroJeitoFuncao = () => {
+        Alert.alert('Título', 'Texto mensagem', [
+            {
+              text: 'Meu texto',
+              onPress: () => Alert.alert('Tchau'),
+              style: 'default',
+            },
+          ])
+    }
+
+    return (
         <View style={styles.container}>
             <Text style={styles.titulo}>Exemplo 3</Text>
             <Text style={styles.texto}>Variáveis vs State</Text>
-            <Button
-            onPress={() => {}}
-            title='Alerta'
-            color="deepskyblue"
-            accessibilityLabel='Mensagem de Alerta'
-            style={styles.botao}/>
+            <View style={styles.botaoAlert}>
+                <Button
+                    onPress={handleOutroJeitoFuncao}
+                    title="Alerta"
+                    color="deepskyblue"
+                    accessibilityLabel="Mensagem de alerta"                  
+                />
+            </View>
+            <Text style={styles.numero}>Valor</Text>
+            <TouchableOpacity style={styles.botao}>
+                <Text style={styles.texto1}>Adicionar +1</Text>
+            </TouchableOpacity>
         </View>
-    )
+    );
 }
 
 export default Exemplo3;
+
--- src/exemplos/ex-03/styles.js
+++ src/exemplos/ex-03/styles.js
@@ -1,36 +1,49 @@
 import { StyleSheet } from 'react-native'; 
-import { RFPercentage } from 'react-native-responsive-fontsize'
+import { RFPercentage } from 'react-native-responsive-fontsize';
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#fff', 
-        padding: 8,
-        flex: 1,
-        alignItems: 'center',
-    
-    },
-    titulo: {
-        fontSize: RFPercentage(3),
-        fontWeight: 'bold',
-        borderWidth: 2,
-        borderColor: 'deepskyblue',
-        marginTop: 10,
-        marginBottom: 10,
-        padding: 8,
-        width: '100%',
-        height: 80,
-        textAlign: 'center',
-        textAlignVertical: 'center',
+        padding: 8, 
+        flex: 1, 
+        alignItems: 'center', 
         borderRadius: 20,
-
-    },
+    }, 
+    titulo: {
+        fontSize: RFPercentage(3), 
+        color: 'deepskyblue', 
+        fontWeight: 'bold', 
+        borderWidth: 2, 
+        borderColor: 'deepskyblue', 
+        marginTop: 10, 
+        marginBottom: 10, 
+        padding: 8, 
+        width: '100%', 
+        height: 80, 
+        textAlign: 'center', 
+        textAlignVertical: 'center', 
+        borderRadius: 20,
+    }, 
     texto: {
-        fontSize: RFPercentage(2.2),
-        color: 'deepskyblue',
-
+        fontSize: RFPercentage(2.2),  
+    }, 
+    botaoAlert: {
+        width: '100%',
+        margin: RFPercentage(1)
     },
-
-    botao: {}
+    botao: {
+        backgroundColor: 'deepskyblue',
+        width: RFPercentage(30),
+        height: RFPercentage(5),
+        borderRadius: 20
+    },
+    texto1: {
+        fontSize: RFPercentage(2),
+        color: '121212',
+        fontWeight: 'bold',
+        alignItems: 'center'
+    }
 });
 
 export default styles;
+
